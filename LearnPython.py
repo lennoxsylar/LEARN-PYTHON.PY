b60a5f000816d5ac0df1b9cc8347296b88a8652e
lennoxsylar <lennoxsylar@gmail.com>
2024-12-07 18:27:59 +0600

    Update LearnPython.py

--- LearnPython.py
+++ LearnPython.py
@@ -1,3 +1,3 @@
-#print text
+#The print() function in Python is a built-in function used to output text, variables, or expressions to the console or standard output. It helps developers display information, debug code, and provide feedback to users.
 print("Hello World")
 #output=Hello World
